aa113f0b46621fb12ce17076f9b6bb0de80681cc
Lingga Adi Pratama <linggaadi4nd@gmail.com>
2018-11-06 15:07:55 +0700

    mailgun fix

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 var nodemailer = require('nodemailer');
 
 var transporter = nodemailer.createTransport({
-  host: "smtp.mailtrap.io",
-  port: 2525,
-  auth: {
-    user: "20fe8b353e0300",
-    pass: "55a38bceaad734"
+  service:'mailgun',
+ auth: {
+    user: 'postmaster@sandbox6b32c2f3d4f1455887c87e62ababfe4f.mailgun.org',
+    pass: 'a3f3da1039c01ad5f8c6c72d1bc3f862-116e1e4d-102d8628'
   }
 });
 
